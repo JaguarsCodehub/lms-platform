128dff58c2deb477b399db387adcc0b1513ba0bb
Jyotindra Tavanoji <tavanojijyotindra@gmail.com>
2025-01-21 10:53:37 +0530

    feat: bored

---
+++ src/actions/groups.ts
@@ -0,0 +1,34 @@
+'use server'
+import { client } from "@/lib/prisma"
+
+export const onGetAffiliateInfo = async (id: string) => {
+    try {
+        const affiliateInfo = await client.affiliate.findUnique({
+            where: {
+                id,
+            },
+            select: {
+                Group: {
+                    select: {
+                        User: {
+                            select: {
+                                firstname: true,
+                                lastname: true,
+                                image: true,
+                                id: true,
+                                stripeId: true,
+                            },
+                        },
+                    },
+                },
+            },
+        })
+
+        if (affiliateInfo) {
+            return {status: 200, user: affiliateInfo}
+        }
+        return {status: 404, message: "Affiliate not found"}
+    } catch (error) {
+        return {status: 400, message: "Affiliate action not working"}
+    }
+}
